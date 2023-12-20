46049fa2bc6478e5a9cd1a4f552dff0cba510ddf
agustrio1 <susantoagustrio16@gmail.com>
2023-12-21 06:15:19 +0700

    add popup

--- pages/components/productdetails/ProductButton.jsx
+++ pages/components/productdetails/ProductButton.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { BiMessage } from "react-icons/bi";
+import { FaTimes } from "react-icons/fa";
 import { useRouter } from "next/router";
 import { auth, onAuthStateChanged } from "../../firebase/init";
 import { useSpring, animated } from "@react-spring/web";
 
 function ProductButton() {
   const [showPopup, setShowPopup] = useState(false);
+  const [hiddenPopup, setHiddenPopup] = useState(false)
   const router = useRouter();
 
   const popupAnimation = useSpring({
-    trensform: showPopup ? "translateY(0)" : "translateY(100%)",
+    transform: showPopup ? "translateY(0)" : "translateY(100%)",
+    opacity: showPopup ? 1 : 0,
   });
 
   useEffect(() => {
@@ -22,11 +25,25 @@ function ProductButton() {
   }, [auth, router]);
 
   const handleBuyNow = () => {
-    router.push("/checkout");
+    setShowPopup(true);
   };
 
   const handleAddToCart = () => {
+    setShowPopup(true);
+  };
+
+  const handleCheckout = () => {
+    setShowPopup(false);
+    router.push("/checkout");
+  };
+
+  const handleCart = () => {
+    setShowPopup(false);
     router.push("/cart");
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
   };
 
   return (
@@ -34,7 +51,8 @@ function ProductButton() {
       <footer
         className={`fixed max-w-[600px] bottom-0 left-0 right-0 p-4 flex justify-between items-center mx-auto`}>
         <button
-          className={`text-lg text-gray-700 p-2 border rounded-md box-border mr-6 ml-6`}>
+          className={`text-lg text-gray-700 p-2 border rounded-md box-border mr-6 ml-6`}
+          onClick={handleClosePopup}>
           <BiMessage />
         </button>
         <button
@@ -48,6 +66,26 @@ function ProductButton() {
           <span className="text-md font-bold">+ Tambah Keranjang</span>
         </button>
       </footer>
+
+      <animated.div
+        style={popupAnimation}
+        className="fixed bottom-0 left-0 right-0 bg-white p-8 mt-8 flexflex-col justify-between items-center mx-auto">
+        <button onClick={handleClosePopup} className="text-lg text-gray-700 mb-4">
+          <FaTimes />
+        </button>
+        <div className="flex">
+          <button
+            onClick={handleCheckout}
+            className={`text-lg p-2 border-green-600 w-full border rounded-md box-border mr-4`}>
+            <span className="text-green-600 text-md font-bold">Beli</span>
+          </button>
+          <button
+            onClick={handleCart}
+            className={`text-lg text-white p-2 w-full bg-green-600 border rounded-md box-border mr-6`}>
+            <span className="text-md font-bold">+ Tambah Keranjang</span>
+          </button>
+        </div>
+      </animated.div>
     </>
   );
 }
