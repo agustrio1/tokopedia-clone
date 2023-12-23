f3d6af5362bc92b863f9138852d9a28d7130cef6
agustrio1 <susantoagustrio16@gmail.com>
2023-12-23 17:54:42 +0700

    update cart

--- pages/keranjang/index.jsx
+++ pages/keranjang/index.jsx
@@ -15,9 +15,8 @@ import {
 
 function Cart() {
   const [cartData, setCartData] = useState([]);
-  const [subtotalArray, setSubtotalArray] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
-  const [selectAll, setSelectAll] = useState(false);
+  const [selectAll, setSelectAll] = useState(true);
 
   useEffect(() => {
     fetchCartData(setCartData);
@@ -27,35 +26,32 @@ function Cart() {
     return price * quantity;
   };
 
-  useEffect(() => {
-    const subTotals = cartData.map((item) =>
-      calculateSubtotal(item.price, item.quantity)
-    );
-    setSubtotalArray(subTotals);
-  }, [cartData]);
-
-  const total = subtotalArray.reduce((acc, curr) => acc + curr, 0);
-
-  const toggleItemSelection = (itemId) => {
-    const updatedselection = selectedItems.includes(itemId)
-      ? selectedItems.filter((id) => id !== itemId)
-      : [...selectedItems, itemId];
-    setSelectedItems(updatedselection);
+  const getTotal = () => {
+    if (selectAll) {
+      return cartData.reduce(
+        (acc, item) => acc + calculateSubtotal(item.price || 0, item.quantity),
+        0
+      );
+    } else {
+      return selectedItems.reduce((acc, itemId) => {
+        const selectedItem = cartData.find((item) => item.id === itemId);
+        return (
+          acc + calculateSubtotal(selectedItem?.price || 0, selectedItem?.quantity || 0)
+        );
+      }, 0);
+    }
   };
 
-  const toggleSelectedAll = () => {
-    setSelectAll(!selectAll);
-    setSelectedItems(selectAll ? [] : cartData.map((item) => item.id));
-  }
+  useEffect(() => {
+    setSelectedItems(cartData.map((item) => item.id));
+  }, [cartData]);
 
   const handleBuyCart = () => {
-    console.log("Total:", total);
-  }
-
-  
+    console.log("Total:", getTotal());
+  };
 
   return (
-    <div className="mt-20 overflow-y-auto max-w-screen-md">
+    <div className="mt-20 overflow-y-auto max-w-screen-[600px]">
       <div className="flex justify-between items-center mb-4">
         <p>
           <span className="font-semibold">
@@ -67,46 +63,83 @@ function Cart() {
         </button>
       </div>
       {cartData.map((item) => (
-        <div key={item.id} className="flex justify-between items-center mb-4">
+        <div
+          key={item.id}
+          className="flex justify-center items-center mb-4 relative">
           <div className="flex items-center">
-          <input
+            <input
               type="checkbox"
-              checked={selectedItems.includes(item.id)}
-              onChange={() => toggleItemSelection(item.id)}
-              className="mr-2"
+              checked={selectAll || selectedItems.includes(item.id)}
+              onChange={() => {
+                const updatedSelection = selectedItems.includes(item.id)
+                  ? selectedItems.filter((id) => id !== item.id)
+                  : [...selectedItems, item.id];
+                setSelectedItems(updatedSelection);
+                setSelectAll(updatedSelection.length === cartData.length);
+              }}
+              className="ml-2 mr-2"
             />
             <img
               src={item.image}
               alt={item.productName}
               style={{ maxWidth: "100px" }}
             />
-            <div className="ml-4">
-              <p>{item.productName}</p>
-              <p>
+            <div className="ml-4 w-[33vw]">
+              <p className="text-sm md:text-md lg:text-lg">
+                {item.productName}
+              </p>
+              <p className="text-sm md:text-md lg:text-lg">
                 Price: Rp. {new Intl.NumberFormat("id-ID").format(item.price)}
               </p>
               <div className="flex items-center mt-4 mx-auto">
-                <button onClick={() => updateItemDecrement(item, setCartData)}>
-              <FaMinus />
-            </button>
-            <span>{item.quantity}</span>
-            <button onClick={() => updateItemIncrement(item, setCartData)}>
-              <FaPlus />
-            </button>
+                <div className="mr-auto">
+                  <FaHeart />
+                </div>
+                <div className="absolute top-1/2 transform -translate-y-1/2 right-0 mt-8">
+                  <div className="flex items-center border border-gray-300 p-1 rounded-md">
+                    <button
+                      onClick={() => updateItemDecrement(item, setCartData, selectedItems, setSelectAll)}
+                      className="ml-auto mr-2">
+                      <FaMinus className="text-green-400 p-1" size={18} />
+                    </button>
+                    <span className="text-sm">{item.quantity}</span>
+                    <button
+                      onClick={() => updateItemIncrement(item, setCartData, selectedItems, setSelectAll)}
+                      className="ml-2">
+                      <FaPlus className="text-green-400 p-1" size={18} />
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       ))}
-      <div className="flex justify-between items-center mt-4">
-        <p className="text-xl">
-          Total: Rp. {new Intl.NumberFormat("id-ID").format(total)}
-        </p>
-        <button
-            className="bg-green-500 text-white px-4 py-2 rounded"
+      <div className="flex justify-between items-center fixed bottom-0 left-0 right-0 max-w-[500px] bg-white p-4 z-10 mx-auto">
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            checked={selectAll}
+            onChange={() => {
+              setSelectAll(!selectAll);
+              setSelectedItems(
+                selectAll ? [] : cartData.map((item) => item.id)
+              );
+            }}
+            className="ml-2 mr-4"
+          />
+          <p className="text-lg">
+            {selectAll ? "Total" : "Subtotal"}: Rp.{" "}
+            {new Intl.NumberFormat("id-ID").format(getTotal())}
+          </p>
+        </div>
+        <div className="flex items-center">
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded ml-4"
             onClick={handleBuyCart}>
             Beli
           </button>
+        </div>
       </div>
     </div>
   );
--- pages/utils/cartLogic.js
+++ pages/utils/cartLogic.js
@@ -1,63 +1,82 @@
 import {
-    retrieveCartWithProductDetails,
-    updateCartItem,
-    deleteCartItem,
-    DeleteAllCart,
-  } from "../firebase/service";
-  
-  export const fetchCartData = async (setCartData) => {
-    try {
-      const data = await retrieveCartWithProductDetails();
-      setCartData(data);
-    } catch (error) {
-      console.error("Error fetching cart data:", error);
-    }
-  };
-  
-  export const updateItemIncrement = async (item, setCartData) => {
-    try {
+  retrieveCartWithProductDetails,
+  updateCartItem,
+  deleteCartItem,
+  DeleteAllCart,
+} from "../firebase/service";
+
+export const fetchCartData = async (setCartData) => {
+  try {
+    const data = await retrieveCartWithProductDetails();
+    setCartData(data);
+  } catch (error) {
+    console.error("Error fetching cart data:", error);
+  }
+};
+
+export const updateItemIncrement = async (
+  item,
+  setCartData,
+  selectedItems,
+  setSelectAll
+) => {
+  try {
+    if (selectedItems && selectedItems.includes(item.id)) {
       const updatedQuantity = item.quantity + 1;
       const updatedSubTotal = item.price * updatedQuantity;
-  
+
       await updateCartItem(item.id, {
         quantity: updatedQuantity,
         subTotal: updatedSubTotal,
       });
-  
+
       fetchCartData(setCartData);
-    } catch (error) {
-      console.error("Error updating cart item:", error);
     }
-  };
-  
-  export const updateItemDecrement = async (item, setCartData) => {
-    try {
-      if (item.quantity > 1) {
-        const updatedQuantity = item.quantity - 1;
-        const updatedSubTotal = item.price * updatedQuantity;
-  
-        await updateCartItem(item.id, {
-          quantity: updatedQuantity,
-          subTotal: updatedSubTotal,
-        });
-  
-        fetchCartData(setCartData);
-      }
-    } catch (error) {
-      console.error("Error updating cart item:", error);
+  } catch (error) {
+    console.error("Error updating cart item:", error);
+  } finally {
+    setSelectAll(false);
+  }
+};
+
+export const updateItemDecrement = async (
+  item,
+  setCartData,
+  selectedItems,
+  setSelectAll
+) => {
+  try {
+    if (selectedItems && selectedItems.includes(item.id) && item.quantity > 1) {
+      const updatedQuantity = item.quantity - 1;
+      const updatedSubTotal = item.price * updatedQuantity;
+
+      await updateCartItem(item.id, {
+        quantity: updatedQuantity,
+        subTotal: updatedSubTotal,
+      });
+
+      fetchCartData(setCartData);
+    } else if (item.quantity === 1) {
+      handleRemoveItem(item.id, setCartData);
+      
     }
-  };
-  
-  export const handleRemoveItem = async (itemId, setCartData) => {
-    await deleteCartItem(itemId);
+  } catch (error) {
+    console.error("Error updating cart item:", error);
+  } finally {
+    setSelectAll(false);
+  }
+};
+
+export const handleRemoveItem = async (itemId, setCartData) => {
+  await deleteCartItem(itemId);
+  fetchCartData(setCartData);
+};
+
+export const handleDeleteAll = async (selectedIds, setCartData) => {
+  try {
+    await DeleteAllCart();
     fetchCartData(setCartData);
-  };
-  
-  export const handleDeleteAll = async (selectedIds, setCartData) => {
-    try {
-      await DeleteAllCart();
-      fetchCartData(setCartData);
-    } catch (error) {
-      console.error("Error deleting all cart items:", error);
-    }
-  };
+  } catch (error) {
+    console.error("Error deleting all cart items:", error);
+  }
+};
