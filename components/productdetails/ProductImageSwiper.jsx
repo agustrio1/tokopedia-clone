bb0b6928740491c1fa8009098c0d21864db096f5
agustrio1 <susantoagustrio16@gmail.com>
2023-12-27 21:58:43 +0700

    fix

--- components/productdetails/ProductImageSwiper.jsx
+++ components/productdetails/ProductImageSwiper.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper/core";
+import React from "react";
+import SwiperCore, { Pagination, Autoplay } from "swiper/core";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
-function ProductImageSwiper({products}) {
+function ProductImageSwiper({ products }) {
   return (
     <Swiper
       width={600}
@@ -17,7 +17,9 @@ function ProductImageSwiper({products}) {
       autoplay={{ delay: 2500, disableOnInteraction: true }}
       style={{ maxWidth: "500px", margin: "auto", textAlign: "center" }}>
       {products.map((product) => (
-        <SwiperSlide key={product.id} style={{ maxWidth: "600px", margin: "auto" }}>
+        <SwiperSlide
+          key={product.id}
+          style={{ maxWidth: "600px", margin: "auto" }}>
           <img
             src={product.image}
             alt={`Product Image`}
@@ -27,7 +29,7 @@ function ProductImageSwiper({products}) {
         </SwiperSlide>
       ))}
     </Swiper>
-  )
+  );
 }
 
-export default ProductImageSwiper
+export default ProductImageSwiper;
