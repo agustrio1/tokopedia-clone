a67c87cad1ee548c62349eae6b4c7181efc9be28
agustrio1 <susantoagustrio16@gmail.com>
2023-12-28 07:17:11 +0700

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="flex flex-col items-center justify-between max-w-screen-md mx-auto">
         <Main />
         <NextScript />
